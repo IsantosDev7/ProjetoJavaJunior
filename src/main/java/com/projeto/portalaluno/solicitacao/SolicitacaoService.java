b3bed45fe61bb91b1b4423b6cc782b6e21b991d1
IsantosDev <israelf.santos.2004@gmail.com>
2026-09-19 01:02:01 -0300

    feature: implementa notificação para o criador da solicitação e para o adminitrador responsável

--- src/main/java/com/projeto/portalaluno/solicitacao/SolicitacaoService.java
+++ src/main/java/com/projeto/portalaluno/solicitacao/SolicitacaoService.java
@@ -1,11 +1,18 @@
 package com.projeto.portalaluno.solicitacao;
 
 import com.projeto.portalaluno.auth.User;
+import com.projeto.portalaluno.auth.UserRepository;
+import com.projeto.portalaluno.auth.UserRole;
 import com.projeto.portalaluno.funcionario.Funcionario;
 import com.projeto.portalaluno.funcionario.FuncionarioRepository;
+import com.projeto.portalaluno.shared.notificacao.NotificacaoService;
+import com.projeto.portalaluno.shared.notificacao.TipoNotificacao;
 import com.projeto.portalaluno.solicitacao.dto.SolicitacaoRequest;
 import com.projeto.portalaluno.solicitacao.dto.SolicitacaoResponse;
 import com.projeto.portalaluno.suporte.Chamado;
+import com.projeto.portalaluno.suporte.ChamadoRepository;
+import jakarta.validation.constraints.Email;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -14,16 +21,21 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 public class SolicitacaoService {
 
     private final SolicitacaoRepository solicitacaoRepository;
     private final FuncionarioRepository funcionarioRepository;
+    private final NotificacaoService notificacaoService;
+    private final UserRepository userRepository;
 
-    public SolicitacaoService(FuncionarioRepository funcionarioRepository, SolicitacaoRepository solicitacaoRepository) {
+    public SolicitacaoService(FuncionarioRepository funcionarioRepository, SolicitacaoRepository solicitacaoRepository, NotificacaoService notificacaoService, UserRepository userRepository) {
         this.funcionarioRepository = funcionarioRepository;
         this.solicitacaoRepository = solicitacaoRepository;
+        this.notificacaoService = notificacaoService;
+        this.userRepository = userRepository;
     }
 
     private SolicitacaoResponse toResponse(Solicitacao solicitacao){
@@ -56,7 +68,13 @@ public class SolicitacaoService {
         solicitacao.setFuncionario(funcionario);
         dadosSolicitacao(solicitacao, request);
 
+        User admin = userRepository.findByRole(UserRole.SUPER_ADMIN)
+                .orElseThrow(()-> new RuntimeException("Administrador não encontrado no sistema"));
+
+
         Solicitacao solicitacaoSalva = solicitacaoRepository.save(solicitacao);
+        notificacaoService.notificarEmail(TipoNotificacao.SOLICITACAO_CRIADA, usuarioLogado, null);
+        notificacaoService.notificarEmail(TipoNotificacao.SOLICITACAO_CRIADA, admin, null);
         return toResponse(solicitacaoSalva);
     }
 
