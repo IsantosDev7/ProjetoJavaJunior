70201eaa8654d691af507378e6b7118b15ca1f71
IsantosDev <israelf.santos.2004@gmail.com>
2026-09-19 01:02:36 -0300

    feature: determina mensagens pré-definidas para uso em notificações

---
+++ src/main/java/com/projeto/portalaluno/shared/notificacao/TipoNotificacao.java
@@ -0,0 +1,22 @@
+package com.projeto.portalaluno.shared.notificacao;
+
+public enum TipoNotificacao {
+    CRIACAO_CONTA("Conta criada", "Sua conta foi criada com sucesso!"),
+    CONTA_APROVADA("Conta Aprovada", "Sua conta foi aprovada."),
+    CONTA_REPROVADA("Conta Reprovada", "Sua conta foi reprovada."),
+    DEFINIR_SENHA("Conta criada com sucesso", "Clique no link abaixo para definir senha."),
+    REDEFINICAO_SENHA("Redefinição de Senha", "Clique no link abaixo para redefinir sua senha:"),
+    NOVA_SOLICITACAO("Nova solicitação recebida","Nova solicitação criado pelo usuário:"),
+    SOLICITACAO_CRIADA("Sua solicitação foi criada","Solicitação criada, aguarde avaliação.");
+
+    private final String subject;
+    private final String body;
+
+    TipoNotificacao(String subject, String body) {
+        this.subject = subject;
+        this.body = body;
+    }
+
+    public String getSubject() { return subject; }
+    public String getBody() { return body; }
+}
